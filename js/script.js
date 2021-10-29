781767bee8a07eddc3eb40c64c70ec73001a56ba
5oN1A <82112240+5oN1A@users.noreply.github.com>
2021-10-29 17:18:47 +0200

    left slider  complete

--- js/script.js
+++ js/script.js
@@ -4,16 +4,36 @@ new Vue({
     el: "#root",
     data: {
         items: [
-            { url: 'img/01.jpg', titolo: 'Svezia', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.' },
-            { url: 'img/02.jpg', titolo: 'Gran Bretagna', text: 'Lorem ipsum' },
-            { url: 'img/03.jpg', titolo: 'Germania', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.' },
-            { url: 'img/04.jpg', titolo: 'Svizzera', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,' },
-            { url: 'img/05.jpg', titolo: 'Paradise', text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,' }
+            { url: 'img/01.jpg', title: 'Svezia', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.' },
+            { url: 'img/02.jpg', title: 'Gran Bretagna', text: 'Lorem ipsum' },
+            { url: 'img/03.jpg', title: 'Germania', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.' },
+            { url: 'img/04.jpg', title: 'Svizzera', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,' },
+            { url: 'img/05.jpg', title: 'Paradise', text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,' }
         ],
-        currentIndex: 0,
+        currentIndex: 4,
 
     },
-    methods: {
 
+    methods: {
+        previousItem: function () {
+            this.currentIndex--;
+
+            if (this.currentIndex < 0) {
+                this.currentIndex = this.items.length - 1;
+            }
+
+            return this.currentIndex
+        },
+
+        nextItem: function () {
+
+            this.currentIndex++;
+
+            if (this.currentIndex > this.items.length - 1) {
+                this.currentIndex = 0;
+            }
+
+            return this.currentIndex
+        }
     }
 });
